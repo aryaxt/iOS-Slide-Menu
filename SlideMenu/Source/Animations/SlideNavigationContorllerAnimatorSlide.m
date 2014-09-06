ffa26fba5b734621115810beaaba42ff58586edd
Aryan <aryaxt@gmail.com>
2014-09-06 15:41:53 -0700

    iOS 8 fixes including bug #66

--- SlideMenu/Source/Animations/SlideNavigationContorllerAnimatorSlide.m
+++ SlideMenu/Source/Animations/SlideNavigationContorllerAnimatorSlide.m
@@ -33,11 +33,11 @@
 
 - (id)init
 {
-	if (self = [self initWithSlideMovement:100])
-	{
-	}
-	
-	return self;
+    if (self = [self initWithSlideMovement:100])
+    {
+    }
+    
+    return self;
 }
 
 - (id)initWithSlideMovement:(CGFloat)slideMovement
@@ -55,98 +55,119 @@
 - (void)prepareMenuForAnimation:(Menu)menu
 {
 	UIViewController *menuViewController = (menu == MenuLeft)
-		? [SlideNavigationController sharedInstance].leftMenu
-		: [SlideNavigationController sharedInstance].rightMenu;
+        ? [SlideNavigationController sharedInstance].leftMenu
+        : [SlideNavigationController sharedInstance].rightMenu;
 	
 	UIInterfaceOrientation orientation= [SlideNavigationController sharedInstance].interfaceOrientation;
 	CGRect rect = menuViewController.view.frame;
 	
-	if (UIInterfaceOrientationIsLandscape(orientation))
-	{
-		if (orientation == UIInterfaceOrientationLandscapeRight)
-		{
-			rect.origin.y = (menu == MenuLeft) ? self.slideMovement*-1 : self.slideMovement;
-		}
-		else
-		{
-			rect.origin.y = (menu == MenuRight) ? self.slideMovement*-1 : self.slideMovement;
-		}
-	}
-	else
-	{
-		if (orientation == UIInterfaceOrientationPortrait)
-		{
-			rect.origin.x = (menu == MenuLeft) ? self.slideMovement*-1 : self.slideMovement;
-		}
-		else
-		{
-			rect.origin.x = (menu == MenuRight) ? self.slideMovement*-1 : self.slideMovement;
-		}
-	}
-	
-	menuViewController.view.frame = rect;
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
+    {
+        rect.origin.x = self.slideMovement;
+    }
+    else
+    {
+        if (UIInterfaceOrientationIsLandscape(orientation))
+        {
+            if (orientation == UIInterfaceOrientationLandscapeRight)
+            {
+                rect.origin.y = (menu == MenuLeft) ? self.slideMovement*-1 : self.slideMovement;
+            }
+            else
+            {
+                rect.origin.y = (menu == MenuRight) ? self.slideMovement*-1 : self.slideMovement;
+            }
+        }
+        else
+        {
+            if (orientation == UIInterfaceOrientationPortrait)
+            {
+                rect.origin.x = (menu == MenuLeft) ? self.slideMovement*-1 : self.slideMovement;
+            }
+            else
+            {
+                rect.origin.x = (menu == MenuRight) ? self.slideMovement*-1 : self.slideMovement;
+            }
+        }
+    }
+    
+    menuViewController.view.frame = rect;
 }
 
 - (void)animateMenu:(Menu)menu withProgress:(CGFloat)progress
 {
-	UIViewController *menuViewController = (menu == MenuLeft)
-		? [SlideNavigationController sharedInstance].leftMenu
-		: [SlideNavigationController sharedInstance].rightMenu;
-	
-	UIInterfaceOrientation orientation= [SlideNavigationController sharedInstance].interfaceOrientation;
-	
-	NSInteger location = (menu == MenuLeft)
-		? (self.slideMovement * -1) + (self.slideMovement * progress)
-		: (self.slideMovement * (1-progress));
-	
-	if (menu == MenuLeft)
-		location = (location > 0) ? 0 : location;
-	
-	if (menu == MenuRight)
-		location = (location < 0) ? 0 : location;
-	
-	CGRect rect = menuViewController.view.frame;
-	
-	if (UIInterfaceOrientationIsLandscape(orientation))
-	{
-		rect.origin.y = (orientation == UIInterfaceOrientationLandscapeRight) ? location : location*-1;
-	}
-	else
-	{
-		rect.origin.x = (orientation == UIInterfaceOrientationPortrait) ? location : location*-1;
-	}
-	
-	menuViewController.view.frame = rect;
+    UIViewController *menuViewController = (menu == MenuLeft)
+        ? [SlideNavigationController sharedInstance].leftMenu
+        : [SlideNavigationController sharedInstance].rightMenu;
+    
+    UIInterfaceOrientation orientation = [SlideNavigationController sharedInstance].interfaceOrientation;
+    
+    NSInteger location = (menu == MenuLeft)
+        ? (self.slideMovement * -1) + (self.slideMovement * progress)
+        : (self.slideMovement * (1-progress));
+    
+    if (menu == MenuLeft)
+        location = (location > 0) ? 0 : location;
+    
+    if (menu == MenuRight)
+        location = (location < 0) ? 0 : location;
+    
+    CGRect rect = menuViewController.view.frame;
+    
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
+    {
+        rect.origin.x = location;
+    }
+    else
+    {
+        if (UIInterfaceOrientationIsLandscape(orientation))
+        {
+            rect.origin.y = (orientation == UIInterfaceOrientationLandscapeRight) ? location : location*-1;
+        }
+        else
+        {
+            rect.origin.x = (orientation == UIInterfaceOrientationPortrait) ? location : location*-1;
+        }
+    }
+    
+    menuViewController.view.frame = rect;
 }
 
 - (void)clear
 {
-	[self clearMenu:MenuLeft];
-	[self clearMenu:MenuRight];
+    [self clearMenu:MenuLeft];
+    [self clearMenu:MenuRight];
 }
 
 #pragma mark - Private Method -
 
 - (void)clearMenu:(Menu)menu
 {
-	UIViewController *menuViewController = (menu == MenuLeft)
-		? [SlideNavigationController sharedInstance].leftMenu
-		: [SlideNavigationController sharedInstance].rightMenu;
-	
-	UIInterfaceOrientation orientation= [SlideNavigationController sharedInstance].interfaceOrientation;
-	
-	CGRect rect = menuViewController.view.frame;
-	
-	if (UIInterfaceOrientationIsLandscape(orientation))
-	{
-		rect.origin.y = 0;
-	}
-	else
-	{
-		rect.origin.x = 0;
-	}
-	
-	menuViewController.view.frame = rect;
+    UIViewController *menuViewController = (menu == MenuLeft)
+    ? [SlideNavigationController sharedInstance].leftMenu
+    : [SlideNavigationController sharedInstance].rightMenu;
+    
+    UIInterfaceOrientation orientation= [SlideNavigationController sharedInstance].interfaceOrientation;
+    
+    CGRect rect = menuViewController.view.frame;
+    
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
+    {
+        rect.origin.x = 0;
+    }
+    else
+    {
+        if (UIInterfaceOrientationIsLandscape(orientation))
+        {
+            rect.origin.y = 0;
+        }
+        else
+        {
+            rect.origin.x = 0;
+        }
+    }
+    
+    menuViewController.view.frame = rect;
 }
 
 @end
--- iOS-Slide-Menu.podspec
+++ iOS-Slide-Menu.podspec
@@ -1,14 +1,14 @@
 Pod::Spec.new do |s|
     s.name = 'iOS-Slide-Menu'
-    s.version = '1.4.2'
+    s.version = '1.4.3'
     s.summary = 'A Slide Menu for iOS'
     s.homepage = 'https://github.com/aryaxt/iOS-Slide-Menu'
     s.license = {
       :type => 'MIT',
       :file => 'License.txt'
     }
-    s.author = {'Aryan Gh' => 'https://github.com/aryaxt/iOS-Slide-Menu'}
-    s.source = {:git => 'https://github.com/aryaxt/iOS-Slide-Menu.git', :tag => '1.4.2'}
+    s.author = {'Aryan Ghassemi' => 'https://github.com/aryaxt/iOS-Slide-Menu'}
+    s.source = {:git => 'https://github.com/aryaxt/iOS-Slide-Menu.git', :tag => '1.4.3'}
     s.platform = :ios, '6.0'
     s.source_files = 'SlideMenu/Source/*.{h,m}', 'SlideMenu/Source/Animations/*.{h,m}', 'SlideMenu/Source/Assets/*.{png}'
     s.resources = ['SlideMenu/Source/Assets/**/*']
