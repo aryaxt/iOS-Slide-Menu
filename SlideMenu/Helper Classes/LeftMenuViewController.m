43f62e7321306d03da881ab1d58b96e942b5e63b
Aryan <aryaxt@gmail.com>
2014-11-28 14:12:13 -0800

    removed border from menus in the demo

--- SlideMenu/Helper Classes/LeftMenuViewController.m
+++ SlideMenu/Helper Classes/LeftMenuViewController.m
@@ -32,9 +32,6 @@
 	
 	UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"leftMenu.jpg"]];
 	self.tableView.backgroundView = imageView;
-	
-	self.view.layer.borderWidth = .6;
-	self.view.layer.borderColor = [UIColor lightGrayColor].CGColor;
 }
 
 #pragma mark - UITableView Delegate & Datasrouce -
