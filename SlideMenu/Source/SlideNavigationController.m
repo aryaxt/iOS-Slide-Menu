3d1d7ae91d325508cd9109b9aed4e7d9fc7b4ca3
Mark Berner <mark2b@gmail.com>
2016-03-03 22:41:12 +0200

    Indentation fixed

--- SlideMenu/Source/SlideNavigationController.m
+++ SlideMenu/Source/SlideNavigationController.m
@@ -445,9 +445,9 @@ static SlideNavigationController *singletonInstance;
 	}
 	else
 	{
-        NSBundle* bundle = [NSBundle bundleForClass:[SlideNavigationController class]];
-        UIImage *image = [UIImage imageNamed:MENU_IMAGE inBundle:bundle compatibleWithTraitCollection:nil];
-        return [[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:self action:selector];
+		NSBundle* bundle = [NSBundle bundleForClass:[SlideNavigationController class]];
+		UIImage *image = [UIImage imageNamed:MENU_IMAGE inBundle:bundle compatibleWithTraitCollection:nil];
+		return [[UIBarButtonItem alloc] initWithImage:image style:UIBarButtonItemStylePlain target:self action:selector];
 	}
 }
 
