be784e228e3331b98b93a68dfa1ef88f8d88b809
Jack Colley <jack.colley@netsells.co.uk>
2016-08-17 13:08:09 +0100

    Fixing issue with menu been non reposnsive
    - Fixing the issue with the menu not been responsive when the navigation controller isn't the root view controller

--- SlideMenu/Source/SlideNavigationController.m
+++ SlideMenu/Source/SlideNavigationController.m
@@ -601,7 +601,7 @@ static SlideNavigationController *singletonInstance;
     self.lastRevealedMenu = menu;
 	
 	[removingMenuViewController.view removeFromSuperview];
-	[self.view.window insertSubview:menuViewController.view atIndex:0];
+	[self.view.superview insertSubview:menuViewController.view atIndex:0];
 
 	[self updateMenuFrameAndTransformAccordingToOrientation];
 	
